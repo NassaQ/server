be2ab37a20344245b3fd58d77006614ae67bffcd
Osama <osama.yousef@tuta.io>
2026-01-20 12:56:39 +0200

    fix[DB]: engine timeout set and pre test connection

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from app.core.config import settings
 
-engine = create_async_engine(settings.SQL_CONNECTION_STRING) # type: ignore
+engine = create_async_engine(
+    settings.SQL_CONNECTION_STRING, # type: ignore
+    connect_args={"timeout": 30},
+    pool_pre_ping=True,
+)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 async def get_db():
